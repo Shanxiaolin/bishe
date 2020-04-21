63a1501ed2ec8141ade57f5d3d84564c47d1ec23
Shanxiaolin <Shanxiaolin163@163.com>
2020-04-21 08:41:26 +0800

    跨域请求设置为允许指定的多个跨域请求

--- server/app.js
+++ server/app.js
@@ -13,18 +13,23 @@ var labRouter = require('./routes/lab');
 
 var app = express();
 
+var url="http://localhost:8081,http://localhost:8081/forgetpwd,http://localhost:8081/updatepwd,http://localhost:8081/student,http://localhost:8081/teacher,http://localhost:8081/admin"
 
-//设置跨域访问
+// 设置跨域访问
 app.all('*', function (req, response, next) {
-  //设置允许跨域的域名，*代表允许任意域名跨域
-  response.header("Access-Control-Allow-Origin", "*");
-  //设置header里边的字段，必须包含下面的两种
-  response.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type");
-  //跨域允许的请求方式
-  response.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-  //设置响应头信息
-   
-  response.header("Content-Type", "application/json");
+  let ol = url.split(",")
+  let check = req.headers.origin
+// indexOf(req.headers.origin)这种写法不支持
+  if (ol.indexOf(check>= 0)) {
+    //设置允许跨域的域名，*代表允许任意域名跨域
+    response.header("Access-Control-Allow-Origin", check);
+    //设置header里边的字段，必须包含下面的两种
+    response.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type");
+    //跨域允许的请求方式
+    response.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    //设置响应头信息
+    response.header("Content-Type", "application/json");
+  }
   next();
 });
 
